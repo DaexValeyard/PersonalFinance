883176d99b66aa48e28604ee33e342bcb962d27a
Daniel Martinez Ruiz <martinez.ruiz_daniel@protonmail.com>
2024-08-22 20:22:13 -0600

    Initial Database Structure

---
+++ PersonalFinance.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalFinance.Domain.Entities;
+
+public class User : IdentityUser
+{
+    [StringLength(40)]
+    public string Name { get; set; } = default!;
+    [StringLength(20)]
+    public string FatherSurname { get; set; } = default!;
+    [StringLength(20)]
+    public string? MotherSurname { get; set; } = default!;
+    [StringLength(13)]
+    public string RFC { get; set; } = default!;
+    [StringLength(18)]
+    public string CURP { get; set; } = default!;
+    [StringLength(10)]
+    public string NSS { get; set; } = default!;
+    public DateOnly DateOfBirth { get; set; }
+    public string? Regime { get; set; } = default!;
+
+    public List<Statement> Statements = [];
+}
---
+++ PersonalFinance.Infrastructure/Persistence/PersonalFinanceDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PersonalFinance.Domain.Entities;
+
+namespace PersonalFinance.Infrastructure.Persistence;
+
+internal class PersonalFinanceDbContext(DbContextOptions<PersonalFinanceDbContext> options)
+    : IdentityDbContext<User>(options)
+{
+    internal DbSet<Statement> Statements { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Statements)
+            .WithOne(s => s.Owner)
+            .HasForeignKey(s => s.OwnerId);
+    }
+}
